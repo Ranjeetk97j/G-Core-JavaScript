80ac65052c3b1f2994a5e259e4ad5d6206f9933c
Ranjeet kumar <116250965+Ranjeetk97j@users.noreply.github.com>
2023-01-14 11:57:54 +0000

    done

--- Programming.js
+++ Programming.js
@@ -65,9 +65,50 @@ Way of adding JavaScript
 iss me humlog html file me javaScript na likh kar humlog alag se koi file 
 banate hai .js name ke aur isko link kar dete hai HTML file se.
 
-.
+:-Inside head Tag
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pratice</title>
+     <script src="p.js" type="text/javascript">
+     </script>
+</head>
+<body>
+ 
+    <h1>
+        style = color:red; I am Heading
+    </h1>
+        
 
+</body>
+</html>
+
+-Inside body Tag
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pratice</title>
+     
+</head>
+<body>
+ 
+    <h1>
+        style = color:red; I am Heading
+    </h1>
+    <script src="p.js" type="text/javascript">
+        
+    </script>
+        
+</body>
+</html>
 
 
 
