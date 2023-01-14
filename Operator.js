a4eabda41372ca4c32bb65d89b3c42a007e6ebe5
Ranjeet kumar <116250965+Ranjeetk97j@users.noreply.github.com>
2023-01-14 17:36:59 +0000

    done

---
+++ Operator.js
@@ -0,0 +1,64 @@
+/*
+JavaSript Operators
+======================
+1).Arithmentic Operators
+2).Comparison (Relational) Operators
+3).Logical Operators
+4).Bitwise Operators
+5).Assignment Operators
+
+>Arithmentic Operators:-
+   (+) (-) (*) (/) (%) (++) (--)
+
+ <script>
+        var a=10; var b=23; var c=56;
+        var d=a+b+c;
+        document.write(d);
+
+    </script>
+
+ <script>
+        var a=10; var b=23; var c=56;
+        var d=a-b-c;
+        document.write(d);
+
+    </script>
+
+>Relational Operators:-
+ (<)  less than      5<2   False
+ (>)  Greater Than   5>2   True
+ (<=) Less than or   5<=2  False
+      equal to 
+ (>=) Greater than   5>=2  True
+      or equal to  
+
+(==)  Equal to       5==2  False
+(!=)  Not equal to   5!=2  True
+(===) Equal value    5===5 True
+      and same type
+(!==) Not Equal value 5!===5 False
+      or Not same type 5!==="5" True
+
+   <script>
+       var a=5;
+       var b=20
+       var c=a>b
+       document.write(c);
+    </script>
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
