d922c7812c75729107ca30131eca3033c35830f6
Ranjeet kumar <116250965+Ranjeetk97j@users.noreply.github.com>
2023-01-14 10:24:53 +0000

    Done

---
+++ Introduction.js
@@ -0,0 +1,37 @@
+/*
+Do you know?
+
+HTML
+CSS
+
+---------------------------------------------------------
+
+What is JavaScript?
+
+JavaScript is the programing language of HTML and the Web. It Makes web page
+dynamic. It is an interpreter programming language with object-oriented capabilities.
+
+----------------------------------------------------------------
+
+JavaScript History
+
+i). 1995 by Brendan Eich (NetScape)
+ii). Mocha
+iii).LiveScript
+iv). JavavScript
+v). ECMA Script
+
+---------------------------------------------------------------
+
+Advantage & Disadvantge
+
+>Advantage:- 1). Client Side Execution
+             2).Validation on Browser
+             3). Easy Language
+ 
+>Disadvantage:- 1). Less Secure
+                2). No Hardware Access
+                            
+
+*/
+
