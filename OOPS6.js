192a49e863a10c8b2dd654154147fb52847b891c
Ranjeet kumar <116250965+Ranjeetk97j@users.noreply.github.com>
2023-01-17 09:35:03 +0000

    done

---
+++ OOPS6.js
@@ -0,0 +1,88 @@
+/*
+Object Oriented Programming
+==========================================================================
+Object-oriented programming (OOP) is a programming language model organized
+around objects rather than "actions" and data rather than logic.
+
+OOPS aisi model hai jisme object se bani hui model hoti hai siwaye functio ke
+
+Concepts of OOP
+==================================
+1). Encapsulation
+2). Abstraction
+3). Inheriance
+4). Polymorbhism
+
+===========================================================================
+Objects
+===========================================================================
+An object is a collection of properties, and a property is an association 
+between a name (or key) and a value. A property's value can be a function,
+in which case the property is known as a method. In addition to objects 
+that are predefined in the browser, you can define your own objects.
+
+var fees={
+    Rahul: 100;|
+    Sumit: 200;|---- properties
+    Rohan: 300;|
+    total: function() {return (100+200+300);}----Methods  
+};
+=============================================
+Type of Objects
+==============================================
+1). User-Defined Objects:- These are custom objects created by the programmer
+    to bringg structure and consistency to a particular programming task.
+
+2). Nativ Objects:- These are provided by the javascript language itself
+    like String, Number, Boolean, Function, Date, Object, Array, Math,
+    RegExp, Error as well as object that allow creation of user-defined
+    objects and composite types.
+    
+3). Host Objects:- These objects are not specified as part of the JavaScript
+    language but that are supported by most host environments, typically
+    browsers like window, navigator.
+
+4). Document Objects:- These are part of the document object model (DOM),
+    as defined by the W3C. These objects presents the programmer with a 
+    structured interface to HTML and XML documents. Access to the document
+    objects is provided by the browser via the document property of the 
+    window object (window.document).
+
+
+============================================================================
+Declaration And Initialization of Object
+============================================================================
+>Using Object Literal
+Syntax:- var object_name ={}
+Ex:-
+   var fees={};
+   Fees["Rahul"]=100; -----> Fees.Rahul=100;
+   Fees["Sumit"]=200; -----> Fees.Sumit=200;
+   Fees["Rohan"]=300; -----> Fees.Rohan=300;
+
+   Fee["Super Man"]=400; ------> Fees.Super Man=400;
+  
+   Multiwork key required quotation.
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
